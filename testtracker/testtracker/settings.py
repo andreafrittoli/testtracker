ee888184eeca56ebcff3ee2a6680583bf67ff9f8
Andrea Frittoli <andrea.frittoli@hp.com>
2014-09-25 17:15:35 +0100

    Database settings and model

--- testtracker/testtracker/settings.py
+++ testtracker/testtracker/settings.py
@@ -58,8 +58,11 @@ WSGI_APPLICATION = 'testtracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ttracker',
+        'USER': 'root',
+        'PASSWORD': 'ttracker',
+        'HOST': 'localhost'
     }
 }
 
